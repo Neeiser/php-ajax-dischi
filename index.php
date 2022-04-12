fad0a5aa3c94482815a0268e8d2fe9f5bf2f4e9e
Neeiser <stortini.study@gmail.com>
2022-04-12 16:56:53 +0200

    some style and structure before structuring the main

--- index.php
+++ index.php
@@ -7,9 +7,30 @@
     <title>PHP Dischi</title>
 </head>
 <body>
+    <header>
+        <div>
+            <img src="./components/style/img/spotify-logo.png" alt="">
+        </div>
+    </header>
+
     <?php
         include __DIR__ . '/components/api/api.php';
         echo $arrdischi;
     ?>
 </body>
 </html>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+header{
+background-color: lightblue;
+}
+img{
+    height: 50px;
+    width: 50px;
+}
+</style>
